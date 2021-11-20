e61ad513ac467f2388e6626684ada77204d7be5d
frnlpz93 <frn.lpznbr@gmail.com>
2021-11-19 23:23:34 -0300

    confirmar 2

--- src/app/pages/registrar/registrar.module.ts
+++ src/app/pages/registrar/registrar.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { RegistrarPageRoutingModule } from './registrar-routing.module';
-
 import { RegistrarPage } from './registrar.page';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    RegistrarPageRoutingModule
+    RegistrarPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [RegistrarPage]
 })
--- src/app/pages/registrar/registrar.page.ts
+++ src/app/pages/registrar/registrar.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-registrar',
@@ -11,5 +13,47 @@ export class RegistrarPage implements OnInit {
 
   ngOnInit() {
   }
+
+  //La variable rut es un controlador de un input
+  nombre = new FormControl('');
+  direccion = new FormControl('');
+  correo = new FormControl('');
+  pass = new FormControl('');
+  rpass = new FormControl('');
+  sede = new FormControl('');
+
+  //Para controlar los elementos como grupo
+  persona = new FormGroup({
+    nombre: new FormControl(''),
+    direccion: new FormControl(''),
+    correo: new FormControl('', Validators.email),
+    pass: new FormControl(''),
+    rpass: new FormControl(''),
+    sede: new FormControl('')
+  });
+
+  //Vamos a crear el btn para la acción grabar
+  listaPersonas = new Array();
+  pers: any;
+  grabar() {
+    //console.log('Comienzo el proceso');
+    //Le entregamos los valores del controlador de grupo a la variable pers
+    this.pers = {
+      nombre: this.persona.controls.nombre.value,
+      direccion: this.persona.controls.direccion.value,
+      correo: this.persona.controls.correo.value,
+      pass: this.persona.controls.pass.value,
+      rpass: this.persona.controls.rpass.value,
+      sede: this.persona.controls.sede.value
+    };
+    console.log(this.pers);
+
+    //Le entregamos los valores de pers a la lista y lo agregamos con un push
+    this.listaPersonas.push(this.pers);
+    //Guardaremos los valores en una variable localStorage (lo maneja el navegador)
+    localStorage.setItem('Datos',JSON.stringify(this.listaPersonas));
+
+  }
+
 
 }
